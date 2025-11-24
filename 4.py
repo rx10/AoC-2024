17fa17d26fb16ec5370a58dfb8c392c442fbbc8e
Charan Tummala <charan.tummala@technumen.com>
2025-11-24 13:59:35 +0530

    third day complete

--- 4.py
+++ 4.py
@@ -5,20 +5,29 @@ def parse_txt(filename: str) -> str:
 
 data = parse_txt("input3.txt")
 
-first_filtered_list = []
-def first_filter(lst):
-    for i in range(len(data)):
-        if (i+3 < len(data) and data[i] == "m" and data[i+1] == "u" and data[i+2] == "l" and data[i+3] == "("):
-            lst.append(data[i:i+14])
+def first_filter(data: list[list[str]]) -> list[str]:
+    data_length = len(data)
+    output_list = []
+    for i in range(data_length):
+        #might be faulty logic
+        if (i+6 < data_length and data[i] == "d" and data[i+1] == "o" and data[i+2] == "n" and data[i+3] == "'" and data[i+4] == "t" and data[i+5] == "(" and data[i+6] == ")"):
+            output_list.append(data[i:i+7])
+        if (i+3 < data_length and data[i] == "d" and data[i+1] == "o" and data[i+2] == "(" and data[i+3] == ")"):
+            output_list.append(data[i:i+4])
+        if (i+3 < data_length and data[i] == "m" and data[i+1] == "u" and data[i+2] == "l" and data[i+3] == "("):
+            output_list.append(data[i:i+14])
+    return output_list
 
-first_filter(first_filtered_list)
-
-second_filtered_list = []
+first_filtered_list = first_filter(data)
 
 elements_we_want = ["m","u","l","0","1","2","3","4","5","6","7","8","9","(",",",")"]
 
-def second_filter(second_filtered_list):
-    for i in first_filtered_list:
+def second_filter(input_list: list[str]) -> list[str]:
+    output_list = []
+    for i in input_list:
+        if i in ["do()","don't()"]:
+            output_list.append(i)
+            continue
         if "," not in i:
             continue
         skip_outer = False
@@ -28,21 +37,42 @@ def second_filter(second_filtered_list):
                 break
             if char == ")":
                 i = i[0:i.index(char)+1]
-                second_filtered_list.append(i)
+                output_list.append(i)
                 break
         if skip_outer:
             continue
+    return output_list
 
-def mul(a,b):
+def mul(a: int,b: int) -> int:
     return (int(a)*int(b))
 
-second_filter(second_filtered_list)
+second_filtered_list = second_filter(first_filtered_list)
 
-def execute_text(lst):
-    ns = {"sum": 0, "mul": mul}
+# def execute_text(lst: list) -> int:
+#     ns = {"sum_total": 0, "mul": mul}
+#     for expr in lst:
+#         # expr should be something like 'mul(3,4)', 'mul(5,2)', etc.
+#         exec("sum_total += " + expr, {}, ns)
+#     return(ns["sum_total"])
+
+def third_part(lst: list) -> int:
+    skip_next = False
+    ns = {"sum_total": 0, "mul": mul}
     for expr in lst:
-        # expr should be something like 'mul(3,4)', 'mul(5,2)', etc.
-        exec("sum += " + expr, {}, ns)
-    print(ns["sum"])
+        print(expr)
+        if expr == "don't()":
+            skip_next = True
+            continue
+        if skip_next:
+            if expr == "do()":
+                skip_next = False
+                continue
+            else:
+                continue
+        if expr[0] == "m":
+            exec("sum_total += " + expr, {}, ns)
+    return ns["sum_total"]
 
-execute_text(second_filtered_list)
+#part 2
+print(third_part(second_filtered_list))
+# def third_filter(input_list: list[int]f) -> list[int]:
